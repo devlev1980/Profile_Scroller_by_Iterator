7b5cd27d4b7c0efb0899c1da9c8c5f13dba7667f
Yevgeny Levin <devlev1980@gmail.com>
2017-12-30 20:07:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+const data = [
+{
+  name:'John Doe',
+  age:32,
+  gender:'male',
+  lookingFor:'female',
+  location:'USA',
+  image:'https://randomuser.me/api/portraits/men/82.jpg'
+},
+{
+  name:'Julia Smith',
+  age:35,
+  gender:'female',
+  lookingFor:'male',
+  location:'Canada',
+  image:'https://randomuser.me/api/portraits/women/3.jpg'
+},
+{
+  name:'Jack Smith',
+  age:38,
+  gender:'male',
+  lookingFor:'female',
+  location:'USA',
+  image:'https://randomuser.me/api/portraits/men/83.jpg'
+}
+];
+
+const profiles = profileIterator(data);
+//Call first profile
+nextProfile();
+
+//Next event
+document.getElementById('next').addEventListener('click',nextProfile);
+
+
+function nextProfile(){
+  const currentProfile = profiles.next().value;
+
+  if (currentProfile!== undefined) {
+    document.getElementById('profileDisplay').innerHTML= `
+  <ul class="list-group">
+    <li class="list-group-item">Name: ${currentProfile.name}</li>
+    <li class="list-group-item">Age: ${currentProfile.age}</li>
+    <li class="list-group-item">Location: ${currentProfile.location}</li>
+    <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingFor}</li>
+  </ul>
+  `;
+  document.getElementById('imageDisplay').innerHTML=`
+  <img src="${currentProfile.image}">
+  `
+  }else{
+    //No more profiles
+    window.location.reload();
+  }
+  
+}
+
+//Profile Iterator
+function profileIterator(profiles) {
+  let nextIndex=0;
+  return {
+    next:function(){
+      return nextIndex < profiles.length ?
+        {value:profiles[nextIndex++],done:false}:
+        {done:true}
+    }
+  };
+}
